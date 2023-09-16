3dc52470bd683fc5ec4bd89a9978bb485e5b60d2
yuriCorrea <fratello.yuri@gmail.com>
2023-09-16 20:39:50 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,32 +1,120 @@
-const botaoAdd = document.querySelector("#btnAdd");
+let botaoAddReceita = document.querySelector("#btnAdd");
+let btnAddDispesa = document.querySelector("#btnDispesa");
+let inputReceita = document.querySelector("#InputaddReceita");
+let inputExtra = document.querySelector("#InputaddExtra");
+let inputDespesa = document.querySelector("#inputDispesa");
+let displaySaldo = document.querySelector("#labelSaldo");
+let displayReceita = document.querySelector("#labelRceita");
+let displayExtra = document.querySelector("#labelextra");
+let displayDespesa = document.querySelector("#labelDispesa");
+let saldoTotal = inputReceita.value + inputExtra.value;
+let despesaTotal = saldoTotal - inputDespesa.value;
 
 
 
-botaoAdd.addEventListener("click", function(e){
+function soma(receita, extra){
+    let saldoTotal = receita + extra;
+    return saldoTotal;
+}
+function subtraiDespesaDoSaldo(totalSaldo,valorDespesa){
+    let subtrair= totalSaldo-valorDespesa;
+    return subtrair;
+}
 
-    let valorInput = document.querySelector("#InputaddReceita");
-    let valueInputReceita = parseInt(valorInput.value);
-
-    document.querySelector("#labelRceita").innerHTML = "$" +valueInputReceita+",00";
-
-    let valorInputExtra = document.querySelector("#InputaddExtra");
-    let valueInputExtra = parseInt(valorInputExtra.value);
 
 
-    document.querySelector("#labelextra").innerHTML = "$" +valueInputExtra+",00";
 
-    let valorInputSaldo =document.querySelector("#labelSaldo");
-    let valueInputSaldo = parseInt(valorInputSaldo.value);
+
+botaoAddReceita.addEventListener("click", ()=>{
+    let valorReceita = parseFloat(inputReceita.value);
+    let valorExtra = parseFloat(inputExtra.value);
     
-    valorInputSaldo.innerHTML= valueInputSaldo=+valueInputExtra+valueInputReceita+",00";
     
+    
+    if(isNaN(valorReceita)){
+        alert("Digite um valor par adicionar Receita!");
+        
+    } 
+    else{
+        displayReceita.innerHTML = "R$"+valorReceita.toFixed(2);
+        displaySaldo.innerHTML = "R$"+valorReceita.toFixed(2);
+    }
+    
+    if(isNaN(valorExtra)){
+        alert("Digite um valor par adicionar Extra!");
+        displaySaldo.innerHTML = "R$00,00";
+        
+    } else{
+        displayExtra.innerHTML = "R$"+valorExtra.toFixed(2);
+        displaySaldo.innerHTML = "R$"+soma(valorReceita,valorExtra).toFixed(2);
+    }
+    
+});
 
-})
+
+
+btnAddDispesa.addEventListener("click", () => {
+    let valorReceita = parseFloat(inputReceita.value);
+    let valorExtra = parseFloat(inputExtra.value);
+    let TatalSaldo = valorReceita +valorExtra;
+
+    let valorDespesa = parseFloat(inputDespesa.value);
+    let categorias = document.querySelector("#selectCategoria");
+    let categoria = categorias.value;
+    let formaPagameto = document.querySelector("#selectformaPagamento");
+    let formasPagamento = formaPagameto.value;
+    if (isNaN(valorDespesa)) {
+        alert("Digite um valor para adicionar Despesa!");
+        return;
+    }
+    if( categoria==0 || formasPagamento ==0){
+        alert("Selecione uma categoria e a forma de pagamento");
+        return;
+    }
+    else{
+        addvalorTabela()
+    }
+    
+    
+    displayDespesa.innerHTML= "R$"+valorDespesa.toFixed(2);
+    displaySaldo.innerHTML= "R$"+subtraiDespesaDoSaldo(TatalSaldo,valorDespesa).toFixed(2);
+});
+
+function addvalorTabela(){
+        
+        let categorias = document.querySelector("#selectCategoria");
+        let categoria = categorias.value;
+
+        let formaPagameto = document.querySelector("#selectformaPagamento");
+        let formasPagamento = formaPagameto.value;
+
+        let inputDispesa = document.querySelector("#inputDispesa");
+        let valorDespesa = parseFloat(inputDispesa.value);
+
+        let tabela = document.querySelector("#tabela");
+
+        if(categoria ==0 && formaPagameto ==0 && valorDespesa ===0 ){
+            console.log("Selecione uma categoria e a forma de pagamento");
+            return;
+        }
+        else {
+            var novaLinha = tabela.insertRow();
+            var novaCategoria = novaLinha.insertCell();
+            var novaFormaPagamento = novaLinha.insertCell();
+            var vDespesa = novaLinha.insertCell();
+
+            novaCategoria.innerHTML = categoria;
+            novaFormaPagamento.innerHTML = formasPagamento;
+            vDespesa.innerHTML = "R$"+valorDespesa.toFixed(2);
+            
+        }
+        
+};   
 
 
 
 
-
-
-
-
+function subtraiDespesaDoSaldo(totalSaldo,valorDespesa){
+    let subtrair= totalSaldo-valorDespesa;
+    return subtrair;
+};
